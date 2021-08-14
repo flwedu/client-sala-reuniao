90e9c918231148238ba4d765efa83cbb0deb6529
flwedu <edutraquino@gmail.com>
2021-08-14 15:30:21 -0300

    add some default routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RoomListComponent } from './room-list/room-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'list', component: RoomListComponent },
+  { path: '', redirectTo: 'list', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
