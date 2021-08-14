3963eebfe20caa72dc02e7482362680af0cb594c
flwedu <edutraquino@gmail.com>
2021-08-14 15:40:01 -0300

    loading data from service

--- src/app/room-list/room-list.component.ts
+++ src/app/room-list/room-list.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RoomService } from '../core/service/room.service';
+import { Room } from '../shared/model/room';
 
 @Component({
   selector: 'app-room-list',
   templateUrl: './room-list.component.html',
-  styleUrls: ['./room-list.component.css']
+  styleUrls: ['./room-list.component.css'],
 })
 export class RoomListComponent implements OnInit {
+  roomList: Room[] = [];
 
-  constructor() { }
+  constructor(
+    private roomService: RoomService,
+    private routes: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.reloadData();
   }
 
+  reloadData() {
+    this.roomService
+      .listAll()
+      .subscribe({
+        next: (results) => (this.roomList = [...results]),
+        error: (err) => console.error(err),
+      });
+  }
 }
