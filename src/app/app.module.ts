0f42971106a739fcf45c03b1f8a8919ba3cd94f0
flwedu <edutraquino@gmail.com>
2021-08-14 10:20:13 -0300

    import generated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RoomService } from './core/service/room.service';
+import { RoomListComponent } from './room-list/room-list.component';
+import { RoomCreateComponent } from './room-create/room-create.component';
+import { RoomDetailsComponent } from './room-details/room-details.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, RoomListComponent, RoomCreateComponent, RoomDetailsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [RoomService],
   bootstrap: [AppComponent],
