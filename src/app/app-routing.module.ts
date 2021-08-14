f039231a6c093747ea6ff4c67bb58f91665008da
flwedu <edutraquino@gmail.com>
2021-08-14 20:33:18 -0300

    add route to RoomCreate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RoomCreateComponent } from './room-create/room-create.component';
 import { RoomListComponent } from './room-list/room-list.component';
 
 const routes: Routes = [
   { path: 'list', component: RoomListComponent },
   { path: '', redirectTo: 'list', pathMatch: 'full' },
+  { path: 'create', component: RoomCreateComponent },
 ];
 
 @NgModule({
