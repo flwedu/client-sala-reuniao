4ec72bbc82c0bd7225ab4d630f75fd781b13fb85
flwedu <edutraquino@gmail.com>
2021-08-14 21:34:38 -0300

    add all missing routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RoomCreateComponent } from './room-create/room-create.component';
+import { RoomDetailsComponent } from './room-details/room-details.component';
 import { RoomListComponent } from './room-list/room-list.component';
+import { RoomUpdateComponent } from './room-update/room-update.component';
 
 const routes: Routes = [
   { path: 'list', component: RoomListComponent },
   { path: '', redirectTo: 'list', pathMatch: 'full' },
   { path: 'create', component: RoomCreateComponent },
+  { path: 'update/:id', component: RoomUpdateComponent },
+  { path: 'details/:id', component: RoomDetailsComponent },
 ];
 
 @NgModule({
