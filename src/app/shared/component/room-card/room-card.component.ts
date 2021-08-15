e5cfaeab2ebbea051c2edbb5e58060c83d83efe6
flwedu <edutraquino@gmail.com>
2021-08-14 21:34:19 -0300

    RoomCard buttons working

--- src/app/shared/component/room-card/room-card.component.ts
+++ src/app/shared/component/room-card/room-card.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RoomService } from 'src/app/core/service/room.service';
 import { Room } from '../../model/room';
 
 @Component({
@@ -10,7 +12,24 @@ export class RoomCardComponent implements OnInit {
   @Input()
   roomBeingShown!: Room;
 
-  constructor() {}
+  constructor(private router: Router, private roomService: RoomService) {}
 
   ngOnInit(): void {}
+
+  goToRoomDetails(id: number) {
+    this.router.navigate(['details', id]);
+  }
+
+  goToRoomUpdate(id: number) {
+    this.router.navigate(['update', id]);
+  }
+
+  deleteRoomById(id: number) {
+    this.roomService.deleteById(id).subscribe({
+      next: (deletedRoom) => {
+        alert(`Room ${deletedRoom.id} deleted!!`);
+      },
+      error: (err) => console.log(err),
+    });
+  }
 }
