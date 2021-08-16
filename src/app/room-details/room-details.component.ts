ea738d27d5d229e4a0e9cc83d29806f1cfbfc247
flwedu <edutraquino@gmail.com>
2021-08-16 17:42:31 -0300

    RoomDetails configured

--- src/app/room-details/room-details.component.ts
+++ src/app/room-details/room-details.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RoomService } from '../core/service/room.service';
+import { Room } from '../shared/model/room';
 
 @Component({
   selector: 'app-room-details',
   templateUrl: './room-details.component.html',
-  styleUrls: ['./room-details.component.css']
+  styleUrls: ['./room-details.component.css'],
 })
 export class RoomDetailsComponent implements OnInit {
+  id: any;
+  room = new Room();
 
-  constructor() { }
+  constructor(
+    private roomService: RoomService,
+    private activeRouter: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.id = this.activeRouter.snapshot.params['id'];
+    this.loadData(this.id);
   }
 
+  loadData(id: any) {
+    this.roomService.findById(id).subscribe({
+      next: (result) => {
+        this.room = result;
+      },
+      error: (err) => console.error(err),
+    });
+  }
+
+  goToList() {
+    this.router.navigate(['list']);
+  }
+
+  goToUpdate() {
+    this.router.navigate(['update', this.id]);
+  }
 }
