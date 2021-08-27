668851f4a44e16bc21e4e3c8762af5873f890590
flwedu <edutraquino@gmail.com>
2021-08-26 21:01:11 -0300

    implementing tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('client-sala-reuniao');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('client-sala-reuniao app is running!');
-  });
 });
