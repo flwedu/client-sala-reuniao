7cebeff4fa964e6284fa56c0c94fff97118f3e05
Eduardo <edutraquino@gmail.com>
2021-12-28 19:15:03 -0300

    create Room Event Service

---
+++ src/app/core/service/room-event.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { RoomEvent } from 'src/app/shared/model/roomEvent';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RoomEventService {
+
+  baseURL = 'http://localhost:8080/api/v1/events';
+
+  constructor(private httpClient: HttpClient) { }
+
+  listAll(): Observable<RoomEvent[]> {
+    return this.httpClient.get<RoomEvent[]>(this.baseURL);
+  }
+
+  findById(id: number): Observable<RoomEvent> {
+    return this.httpClient.get<RoomEvent>(this.baseURL + '/' + id);
+  }
+
+  findByRoomId(roomId: number): Observable<RoomEvent[]> {
+    return this.httpClient.get<RoomEvent[]>(`${this.baseURL}/rooms/${roomId}/events`)
+  }
+
+  // save(roomEvent: RoomEvent): Observable<RoomEvent> {
+  //   return this.httpClient.post<RoomEvent>(this.baseURL, roomEvent);
+  // }
+
+  // update(id: number, roomEvent: RoomEvent): Observable<RoomEvent> {
+  //   return this.httpClient.put<RoomEvent>(this.baseURL + '/' + id, roomEvent);
+  // }
+
+  // deleteById(id: number): Observable<any> {
+  //   return this.httpClient.delete<any>(this.baseURL + '/' + id);
+  // }
+}
